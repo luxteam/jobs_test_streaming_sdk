5d8076990410f25d496d49fa988e7ce825d0c44f
Ilia Nemankov <INemankov@luxoft.com>
2021-12-02 18:14:30 +0300

    Copy necessary keys in client json

--- scripts/unite_case_results.py
+++ scripts/unite_case_results.py
@@ -6,10 +6,10 @@ from glob import glob
 
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.path.pardir)))
-from jobs_launcher.core.config import SESSION_REPORT, TEST_REPORT_NAME_COMPARED
+from jobs_launcher.core.config import SESSION_REPORT, TEST_REPORT_NAME_COMPARED, SCREENS_COLLECTION_KEY, VIDEO_KEY
 
 
-KEYS_TO_COPY = ["min_server_latency", "max_server_latency", "median_server_latency", "server_trace_archive", "firstinstance_server", "secondinstance_server", "iperf_server", "used_memory_server"]
+KEYS_TO_COPY = ["min_server_latency", "max_server_latency", "median_server_latency", "server_trace_archive", "firstinstance_server", "secondinstance_server", "iperf_server", "used_memory_server", "android_log"]
 
 
 def get_test_status(test_status_one, test_status_two):
@@ -85,6 +85,12 @@ if __name__ == '__main__':
 
                         target_file_content[i]["script_info"] = format_script_info(target_file_content[i]["script_info"])
 
+                        if VIDEO_KEY in source_file_content[i]:
+                            target_file_content[i]["android_" + VIDEO_KEY] += source_file_content[i][VIDEO_KEY]
+
+                        if SCREENS_COLLECTION_KEY in source_file_content[i]:
+                            target_file_content[i]["android_" + SCREENS_COLLECTION_KEY] += source_file_content[i][SCREENS_COLLECTION_KEY]
+
                     with open(target_file_path, "w", encoding="utf8") as f:
                         json.dump(target_file_content, f, indent=4, sort_keys=True)
 
@@ -129,6 +135,12 @@ if __name__ == '__main__':
                             target_group_data["render_results"][i]["script_info"].extend(source_group_data["render_results"][i]["script_info"])
 
                             target_group_data["render_results"][i]["script_info"] = format_script_info(target_group_data["render_results"][i]["script_info"])
+
+                            if VIDEO_KEY in source_group_data["render_results"][i]:
+                                target_group_data["render_results"][i]["android_" + VIDEO_KEY] += source_group_data["render_results"][i][VIDEO_KEY]
+
+                            if SCREENS_COLLECTION_KEY in source_group_data["render_results"][i]:
+                                target_group_data["render_results"][i]["android_" + SCREENS_COLLECTION_KEY] += source_group_data["render_results"][i][SCREENS_COLLECTION_KEY] 
 
                     with open(target_file_path, "w", encoding="utf8") as f:
                         json.dump(target_file_content, f, indent=4, sort_keys=True)
