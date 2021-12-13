e0e12783ded47a098ebdc82c9d0a22f7a09fbbf4
Ilia Nemankov <INemankov@luxoft.com>
2021-12-13 20:59:50 +0300

    Small fixes

--- jobs/Scripts/mc_actions.py
+++ jobs/Scripts/mc_actions.py
@@ -100,7 +100,7 @@ class RecordVideo(Action):
         recorder = FFmpeg()
         self.logger.info("Start to record video")
 
-        recorder.options("-f gdigrab -video_size {resolution} -i desktop -f dshow -i audio=\"{audio_device_name}\" -t {time} -q:v 3 -pix_fmt yuv420p {video}"
+        recorder.options("-f gdigrab -video_size {resolution} -i desktop -f dshow -t {time} -q:v 3 -pix_fmt yuv420p {video}"
             .format(resolution=self.resolution, audio_device_name=self.audio_device_name, time=time_flag_value, video=video_full_path))
 
         self.logger.info("Finish to record video")
