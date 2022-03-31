0b63d577168fd08c22df3a8492fe922ba37bbae6
Speedwag00n <INemankov@luxoft.com>
2022-03-31 13:37:09 +0300

    Fix prepare_test_cases.py script

--- scripts/prepare_test_cases.py
+++ scripts/prepare_test_cases.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     target_package_name = "regression-{}.json".format(args.os_name.lower())
     renamed_package_name = "regression.json"
 
-    for path, dirs, files in os.walk(os.path.join("..", "jobs", "Tests")):
+    for path, dirs, files in os.walk(os.path.join("..", "jobs")):
         for json_file in files:
             if json_file == target_json_name:
                 copyfile(os.path.join(path, json_file), os.path.join(path, renamed_json_name))
