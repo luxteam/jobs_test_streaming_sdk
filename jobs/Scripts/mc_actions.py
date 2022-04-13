45b9e814dbfa8d76f2c24f0e3ae51bea9361fcb0
Speedwag00n <INemankov@luxoft.com>
2022-04-13 18:35:26 +0300

    Track memory after video recording (second client)

--- jobs/Scripts/mc_actions.py
+++ jobs/Scripts/mc_actions.py
@@ -114,6 +114,9 @@ class RecordVideo(Action):
 
         self.sock.send("done".encode("utf-8"))
 
+        if self.params["args"].track_used_memory:
+            track_used_memory(self.params["case"], "client") 
+
 
 class Encryption(Action):
     def parse(self):
@@ -136,8 +139,7 @@ class Finish(Action):
         pass
 
     def execute(self):
-        if self.params["args"].track_used_memory:
-            track_used_memory(self.params["case"], "client")
+        pass
 
     def analyze_result(self):
         self.state.finish_command_received = True
