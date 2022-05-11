bc879b78fd98c4a385f9ee49ff2015210861cf94
Speedwag00n <INemankov@luxoft.com>
2022-05-11 22:32:32 +0300

    Add ignore_encryption_check key

--- jobs/Scripts/utils.py
+++ jobs/Scripts/utils.py
@@ -431,9 +431,8 @@ def analyze_encryption(case, execution_type, transport_protocol, is_encrypted, m
         main_logger.info("Ignore encryption analyzing due to expected problems")
         return
 
-    # do not check encryption if problems case has wrong passphrase
-    if "has_wrong_passphrase" in case and case["has_wrong_passphrase"]:
-        main_logger.info("Ignore encryption analyzing due to wrong passphrase")
+    if "ignore_encryption_check" in case and case["ignore_encryption_check"]:
+        main_logger.info("Force ignore encryption analyzing")
         return
 
     encryption_is_valid = validate_encryption(execution_type, transport_protocol, "src", is_encrypted, address)
